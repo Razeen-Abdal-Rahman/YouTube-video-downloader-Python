9d2254198cf3200ca7e49f051564f09bc15a1804
Razeen-Abdal-Rahman <98765619+Razeen-Abdal-Rahman@users.noreply.github.com>
2022-02-20 21:04:45 +0000

    Added video downloader

--- Downloader.py
+++ Downloader.py
@@ -5,6 +5,19 @@ import youtube_dl
 def apis():
     """A CLI for getting transcriptions of YouTube videos"""
 
+@click.argument('link')
+@apis.command()
+def download_video(link): #function to downlaod videos, run the file adding downlaod-video "youtube video link"
+   ydl_opts = {
+       'format': 'mp4',
+       'outtmpl': "./%(id)s.%(ext)s",
+   }
+   _id = link.strip()
+   meta = youtube_dl.YoutubeDL(ydl_opts).extract_info(_id)
+   save_location = meta['id'] + ".mp4"
+   print(save_location)
+   return save_location
+
 
 
 def main():
